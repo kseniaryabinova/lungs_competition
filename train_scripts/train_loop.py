4a8c4769524601750d4ebe46ab1dd04b046a2f4b
kseniaryabinova <ryabinova.ka@mail.ru>
2021-01-31 11:22:16 +0300

    resnet18 lb 0.826 after addition clahe and griddistr augs

--- train_scripts/train_loop.py
+++ train_scripts/train_loop.py
@@ -4,7 +4,7 @@ import shutil
 import time
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
-# os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':64:8'
 
 import pandas as pd
 import numpy as np
@@ -32,14 +32,14 @@ writer = SummaryWriter(log_dir='tensorboard_runs', filename_suffix=str(time.time
 df = pd.read_csv('train_with_split.csv')
 train_df = df[df['split'] == 1]
 train_image_transforms = alb.Compose([
-    # alb.CLAHE(p=0.5),
-    # alb.GridDistortion(p=0.5),
+    alb.CLAHE(p=0.5),
+    alb.GridDistortion(p=0.5),
     alb.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
     ToTensorV2()
 ])
 train_set = ImageDataset(train_df, train_image_transforms, '../ranzcr/train', width_size=128)
 # train_set = ImageDataset(train_df, train_image_transforms, '../dataset/train', width_size=128)
-train_loader = DataLoader(train_set, batch_size=64, shuffle=True, num_workers=48, pin_memory=True)
+train_loader = DataLoader(train_set, batch_size=64, shuffle=True, num_workers=48, pin_memory=True, prefetch_factor=4)
 
 val_df = df[df['split'] == 0]
 val_image_transforms = alb.Compose([
@@ -48,14 +48,14 @@ val_image_transforms = alb.Compose([
 ])
 val_set = ImageDataset(val_df, val_image_transforms, '../ranzcr/train', width_size=128)
 # val_set = ImageDataset(val_df, val_image_transforms, '../dataset/train', width_size=128)
-val_loader = DataLoader(val_set, batch_size=64, num_workers=48, pin_memory=True)
+val_loader = DataLoader(val_set, batch_size=64, num_workers=48, pin_memory=True, prefetch_factor=4)
 
 os.makedirs('checkpoints', exist_ok=True)
 
-scaler = GradScaler()
-# scaler = None
+scaler = None
 model = ResNet18(11, 1, pretrained_backbone=True, mixed_precision=True)
 if torch.cuda.device_count() > 1:
+    scaler = GradScaler()
     model = torch.nn.DataParallel(model)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -74,7 +74,6 @@ model = model.to(device)
 for epoch in range(40):
     total_train_loss, train_avg_auc, train_auc, train_duration = one_epoch_train(
         model, train_loader, optimizer, criterion, device, scaler)
-
     total_val_loss, val_avg_auc, val_auc, val_duration = eval_model(
         model, val_loader, device, criterion, scaler)
 
