0e447baef55eb171f21f05cd4aa943d36dbbd53e
kseniaryabinova <ryabinova.ka@mail.ru>
2021-02-23 14:10:02 +0300

    started to implement pseudolabels

--- train_scripts/train_functions.py
+++ train_scripts/train_functions.py
@@ -196,13 +196,13 @@ def one_epoch_train_2_stage(teacher, student, train_loader, optimizer, criterion
     total_loss /= iter_counter / iters_to_accumulate
     predictions = np.array(predictions, dtype=np.float)
     ground_truth = np.array(ground_truth, dtype=np.float)
-    avg_auc, aucs = get_metric(predictions, ground_truth)
+    avg_auc, aucs, _ = get_metric(predictions, ground_truth)
 
     return total_loss, avg_auc, aucs, (predictions, ground_truth), time.time() - start_time
 
 
 def eval_model_2_stage(model, val_loader: DataLoader,
-               device: torch.device, criterion, scaler):
+                       device: torch.device, criterion, scaler):
     predictions = []
     ground_truth = []
     total_loss = 0
@@ -234,6 +234,82 @@ def eval_model_2_stage(model, val_loader: DataLoader,
     total_loss /= iter_counter
     predictions = np.array(predictions, dtype=np.float)
     ground_truth = np.array(ground_truth, dtype=np.float)
-    avg_auc, aucs = get_metric(predictions, ground_truth)
+    avg_auc, aucs, _ = get_metric(predictions, ground_truth)
 
     return total_loss, avg_auc, aucs, (predictions, ground_truth), time.time() - start_time
+
+
+class UnlabeledLoss(nn.Module):
+    def __init__(self, t1, t2, alpha, class_weights=None):
+        super(UnlabeledLoss, self).__init__()
+        self.t1 = t1
+        self.t2 = t2
+        self.alpha = alpha
+        self.class_weights = class_weights
+        self.unlabeled_loss = torch.nn.BCEWithLogitsLoss(pos_weight=class_weights)
+
+    def get_weight(self, epoch):
+        if epoch < self.t1:
+            return 0.
+        elif self.t1 <= epoch < self.t2:
+            return (epoch - self.t1) / (self.t2 - self.t1) * self.alpha
+        elif self.t2 <= epoch:
+            return self.alpha
+
+    def get_labels_out_of_predictions(self, predictions):
+        return (predictions > 0.5).float()
+
+    def forward(self, epoch, predictions):
+        weight = self.get_weight(epoch)
+        labels = self.get_labels_out_of_predictions(predictions)
+        return weight * self.unlabeled_loss(predictions, labels)
+
+
+def train_one_epoch_pseudolabel(epoch: int, model: nn.Module,
+                                train_loader_with_labels: DataLoader,
+                                train_loader_without_labels: DataLoader,
+                                optimizer: torch.optim.Optimizer,
+                                labeled_loss: nn.Module, unlabeled_loss: nn.Module,
+                                device, scaler, iters_to_accumulate, clip_grads):
+    start_time = time.time()
+    model.train()
+    optimizer.zero_grad()
+
+    if unlabeled_loss.get_weight(epoch) == 0:
+        train_loss, train_avg_auc, train_auc, train_rocs, train_data_pr, _ = one_epoch_train(
+            model, train_loader_with_labels, optimizer, labeled_loss,
+            device, scaler, iters_to_accumulate, clip_grads)
+    else:
+        for i, unlabeled_inputs in enumerate(train_loader_without_labels):
+            if scaler is not None:
+                with autocast():
+                    outputs = model(unlabeled_inputs.to(device))
+                    loss = unlabeled_loss(epoch, outputs)
+                    loss = loss / iters_to_accumulate
+                scaler.scale(loss).backward()
+
+                if (i + 1) % iters_to_accumulate == 0:
+                    if clip_grads:
+                        scaler.unscale_(optimizer)
+                        torch.nn.utils.clip_grad_norm_(model.parameters(), 1000)
+                    scaler.step(optimizer)
+                    scaler.update()
+                    optimizer.zero_grad()
+
+            else:
+                outputs = model(unlabeled_inputs.to(device))
+                loss = unlabeled_loss(epoch, outputs)
+                loss = loss / iters_to_accumulate
+                loss.backward()
+
+                if (i + 1) % iters_to_accumulate == 0:
+                    if clip_grads:
+                        torch.nn.utils.clip_grad_norm_(model.parameters(), 1000)
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+        train_loss, train_avg_auc, train_auc, train_rocs, train_data_pr, _ = one_epoch_train(
+            model, train_loader_with_labels, optimizer, labeled_loss,
+            device, scaler, iters_to_accumulate, clip_grads)
+
+    return train_loss, train_avg_auc, train_auc, train_rocs, train_data_pr, time.time() - start_time
